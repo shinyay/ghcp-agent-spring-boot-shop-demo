0aaaa79cc5b9d7c970f23f31552d4c0c935ed5ee
shinyay <shinya.com@gmail.com>
2025-03-16 11:42:22 +0900

    Initial Commit

---
+++ src/test/java/com/github/shinyay/demo/agent/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.shinyay.demo.agent.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
